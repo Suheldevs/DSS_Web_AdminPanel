8b10ee618be5c3dea53b0f9005f81d6454435296
Suheldevs <mohdsuhel.dev@gmail.com>
2025-08-26 18:05:45 +0530

    admin panel code

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageHeader from '../components/PageHeader'
+
+function Dashboard() {
+  return (
+    <div>
+      <PageHeader title='Dashboard' />
+      Dashboard</div>
+  )
+}
+
+export default Dashboard
